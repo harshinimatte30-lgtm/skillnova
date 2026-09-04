7e367df55fd639d183136023d67324f656424053
Harshini Matte <harshinimatte30@gmail.com>
2026-09-04 09:02:40 +0530

    Fix deployment settings import

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI, Depends, HTTPException, Header
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
-from .db import Base, engine, get_db
+from .db import Base, engine, get_db, settings
 from .models import (
     User,
     Profile,
